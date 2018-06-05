307994c85f0ddfcf21e95bfe3f651792155fdac3
David <davidgcastro93@gmail.com>
2018-06-05 16:00:55 -0400

    Product model routes#42 (#46)
    * Finished product model
    
    * finished all products api route
    
    * added individual product api
    
    * updated this feature branch
    
    * fixed random bugs on merge

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,11 @@
-const router = require('express').Router()
-module.exports = router
+const router = require('express').Router();
+module.exports = router;
 
-router.use('/users', require('./users'))
+router.use('/users', require('./users'));
+router.use('/products', require('./products'));
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
---
+++ server/api/products.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+const { Product } = require('../db/models');
+
+router.get('/', (req, res, next) => {
+  Product.findAll()
+    .then(products => res.json(products))
+    .catch(next);
+});
+
+router.get('/:id', (req, res, next) => {
+  let id = req.params.id;
+  Product.find({
+    where: {
+      id
+    }
+  })
+    .then(product => res.json(product))
+    .catch(next);
+});
+
+module.exports = router;
