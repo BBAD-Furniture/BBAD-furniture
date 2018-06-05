894171d5b33ee69d523d3e99ff3a51123b05a8df
David <davidgcastro93@gmail.com>
2018-06-05 17:07:08 -0400

    finished product routes

--- server/api/products.js
+++ server/api/products.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 
-const { Product } = require('../db/models');
+const { Product, Review } = require('../db/models');
 
 router.get('/', (req, res, next) => {
-  Product.findAll()
+  Product.findAll({
+    include: {
+      model: Review
+    }
+  })
     .then(products => res.json(products))
     .catch(next);
 });
@@ -19,4 +23,18 @@ router.get('/:id', (req, res, next) => {
     .catch(next);
 });
 
+router.post('/', (req, res, next) => {
+  Product.create(req.body)
+    .then(newProduct => res.status(201).send(newProduct))
+    .catch(next);
+});
+
+router.delete('/:id', (req, res, next) => {
+  let id = req.params.id;
+
+  Product.findById(id)
+    .then(product => product.destroy())
+    .then(() => res.send('deleted!'))
+    .catch(next);
+});
 module.exports = router;
