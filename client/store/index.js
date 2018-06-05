66e7c625e8a5706b8be57a74c6fcc8197896ad60
Abdul Ghaffar <aghaffar570@gmail.com>
2018-06-05 16:03:32 -0400

    productlist component and reducer initialized (#45)

--- client/store/index.js
+++ client/store/index.js
@@ -1,15 +1,15 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import createLogger from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import user from './user'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import createLogger from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import user from './user';
+import product from './productList';
 
-const reducer = combineReducers({user})
-const middleware = composeWithDevTools(applyMiddleware(
-  thunkMiddleware,
-  createLogger({collapsed: true})
-))
-const store = createStore(reducer, middleware)
+const reducer = combineReducers({ user, product });
+const middleware = composeWithDevTools(
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+);
+const store = createStore(reducer, middleware);
 
-export default store
-export * from './user'
+export default store;
+export * from './user';
