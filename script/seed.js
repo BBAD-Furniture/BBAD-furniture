265f46eb1851e60141bf497efb5f7c3d46323f50
Bushra Taimoor <bushra376@hotmail.com>
2018-06-05 23:24:06 -0400

    backend auth work

--- script/seed.js
+++ script/seed.js
@@ -7,73 +7,69 @@ const assc = require('../server/db/models');
 const Promise = db.Promise; // gives us Promise.map
 
 async function seed() {
-    await db.sync({ force: false });
-    const product = await seedProduct();
-    console.log('Seeded', product.length, 'product.');
-    const user = await seedUser();
-    console.log('Seeded', user.length, 'user.');
-    const review = await seedReview();
-    console.log('Seeded', review.length, 'review.');
-    const cart = await seedCart();
-    console.log('Seeded', cart.length, 'cart.');
+  await db.sync({ force: true });
+  const product = await seedProduct();
+  console.log('Seeded', product.length, 'product.');
+  const user = await seedUser();
+  console.log('Seeded', user.length, 'user.');
+  const review = await seedReview();
+  console.log('Seeded', review.length, 'review.');
+  const cart = await seedCart();
+  console.log('Seeded', cart.length, 'cart.');
 }
 
 function seedUser(users) {
-    return Promise.all(
-        new Array(10).fill(1).map(() =>
-            User.create({
-                firstName: faker.name.firstName(),
-                lastName: faker.name.lastName(),
-                email: faker.internet.email(),
-                password: faker.internet.password()
-            })
-        )
-    );
+  return Promise.all(
+    new Array(10).fill(1).map(() =>
+      User.create({
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password()
+      }))
+  );
 }
 
 function seedProduct(products) {
-    return Promise.all(
-        new Array(50).fill(1).map(() =>
-            Product.create({
-                name: faker.name.firstName(),
-                description: faker.lorem.text(),
-                price: (Math.random() * 100).toFixed(2),
-                category: [faker.commerce.product()],
-                image: faker.image.image(),
-                color: faker.commerce.color(),
-                quantity: Math.floor(Math.random() * 100)
-            })
-        )
-    );
+  return Promise.all(
+    new Array(50).fill(1).map(() =>
+      Product.create({
+        name: faker.name.firstName(),
+        description: faker.lorem.text(),
+        price: (Math.random() * 100).toFixed(2),
+        category: [faker.commerce.product()],
+        image: faker.image.image(),
+        color: faker.commerce.color(),
+        quantity: Math.floor(Math.random() * 100)
+      }))
+  );
 }
 
 function seedReview(reviews) {
-    return Promise.all(
-        new Array(60).fill(1).map(() =>
-            Review.create({
-                review: faker.lorem.paragraph(),
-                userId: Math.floor(Math.random() * 10) + 1,
-                productId: Math.floor(Math.random() * 50) + 1
-            })
-        )
-    );
+  return Promise.all(
+    new Array(60).fill(1).map(() =>
+      Review.create({
+        review: faker.lorem.paragraph(),
+        userId: Math.floor(Math.random() * 10) + 1,
+        productId: Math.floor(Math.random() * 50) + 1
+      }))
+  );
 }
 
 function seedCart(carts) {
-    return Promise.all(
-        new Array(5).fill(1).map(() =>
-            Cart.create({
-                userId: Math.floor(Math.random() * 10) + 1,
-                productId: Math.floor(Math.random() * 50) + 1
-            })
-        )
-    );
+  return Promise.all(
+    new Array(5).fill(1).map(() =>
+      Cart.create({
+        userId: Math.floor(Math.random() * 10) + 1,
+        productId: Math.floor(Math.random() * 50) + 1
+      }))
+  );
 }
 
 const exit = () => process.exit(0);
 const die = err => {
-    console.error(err);
-    process.exit(1);
+  console.error(err);
+  process.exit(1);
 };
 
 if (module === require.main) seed().then(exit, die);
--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,55 +3,53 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const User = db.define('user', {
-    firstName: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    lastName: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    email: {
-        type: Sequelize.STRING,
-        unique: true,
-        allowNull: false,
-        validate: {
-            isEmail: true
-        }
-    },
-    password: {
-        type: Sequelize.STRING,
-        // Making `.password` act like a func hides it when serializing to JSON.
-        // This is a hack to get around Sequelize's lack of a "private" option.
-        get() {
-            return () => this.getDataValue('password');
-        }
-    },
-    salt: {
-        type: Sequelize.STRING,
-        // Making `.salt` act like a function hides it when serializing to JSON.
-        // This is a hack to get around Sequelize's lack of a "private" option.
-        get() {
-            return () => this.getDataValue('salt');
-        }
-    },
-    googleId: {
-        type: Sequelize.STRING
-    },
-    isAdmin: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false
-    },
-    fullName: {
-        type: Sequelize.VIRTUAL,
-        get() {
-            return (
-                this.getDataValue('firstName') +
-                ' ' +
-                this.getDataValue('lastName')
-            );
-        }
+  firstName: {
+    type: Sequelize.STRING
+    // allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING
+    // allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      isEmail: true
     }
+  },
+  password: {
+    type: Sequelize.STRING,
+    // Making `.password` act like a func hides it when serializing to JSON.
+    // This is a hack to get around Sequelize's lack of a "private" option.
+    get() {
+      return () => this.getDataValue('password');
+    }
+  },
+  salt: {
+    type: Sequelize.STRING,
+    // Making `.salt` act like a function hides it when serializing to JSON.
+    // This is a hack to get around Sequelize's lack of a "private" option.
+    get() {
+      return () => this.getDataValue('salt');
+    }
+  },
+  googleId: {
+    type: Sequelize.STRING
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  }
+  //   fullName: {
+  //     type: Sequelize.VIRTUAL,
+  //     get() {
+  //       return (
+  //         this.getDataValue('firstName') + ' ' + this.getDataValue('lastName')
+  //       );
+  //     }
+  //   }
 });
 
 module.exports = User;
@@ -60,32 +58,32 @@ module.exports = User;
  * instanceMethods
  */
 User.prototype.correctPassword = function(candidatePwd) {
-    return User.encryptPassword(candidatePwd, this.salt()) === this.password();
+  return User.encryptPassword(candidatePwd, this.salt()) === this.password();
 };
 
 /**
  * classMethods
  */
 User.generateSalt = function() {
-    return crypto.randomBytes(16).toString('base64');
+  return crypto.randomBytes(16).toString('base64');
 };
 
 User.encryptPassword = function(plainText, salt) {
-    return crypto
-        .createHash('RSA-SHA256')
-        .update(plainText)
-        .update(salt)
-        .digest('hex');
+  return crypto
+    .createHash('RSA-SHA256')
+    .update(plainText)
+    .update(salt)
+    .digest('hex');
 };
 
 /**
  * hooks
  */
 const setSaltAndPassword = user => {
-    if (user.changed('password')) {
-        user.salt = User.generateSalt();
-        user.password = User.encryptPassword(user.password(), user.salt());
-    }
+  if (user.changed('password')) {
+    user.salt = User.generateSalt();
+    user.password = User.encryptPassword(user.password(), user.salt());
+  }
 };
 
 User.beforeCreate(setSaltAndPassword);
