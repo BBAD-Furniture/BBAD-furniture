9c23a242e18b4baa3befc9fdeb2f35d044c15998
David <davidgcastro93@gmail.com>
2018-06-06 17:01:51 -0400

    deleted conflicts

--- client/components/ProductList.js
+++ client/components/ProductList.js
@@ -9,47 +9,13 @@ import '../styles/productList.css';
  * COMPONENT
  */
 export const ProductList = props => {
-<<<<<<< HEAD
-  let products = props.products;
-  return (
-    <div className="flexWrap">
-      <div className="sidebar">
-        <Sidebar />
-      </div>
-      <div className="product-main">
-        {products &&
-          products.map(item => {
-            return (
-              <div className="product-item" key={item.id}>
-                <Link to={`/products/${item.id}`}>
-                  <img src={item.image} />
-                </Link>
-                <h3>{item.name}</h3>
-                <p>
-                  <strong>Price:</strong> ${item.price}
-                </p>
-                <h3>
-                  <strong>Categories:</strong>
-                </h3>
-                <ul>
-                  {item.category.map((category, idx) => {
-                    return <li key={idx}>{category}</li>;
-                  })}
-                </ul>
-                <p>
-                  <strong>Color:</strong> {item.color}
-                </p>
-              </div>
-            );
-          })}
-      </div>
-    </div>
-  );
-=======
 	let products = props.products;
 	return (
-		<div>
-			<div>
+		<div className="flexWrap">
+			<div className="sidebar">
+				<Sidebar />
+			</div>
+			<div className="product-main">
 				{products &&
 					products.map(item => {
 						return (
@@ -58,10 +24,6 @@ export const ProductList = props => {
 									<img src={item.image} />
 								</Link>
 								<h3>{item.name}</h3>
-								<p>
-									<strong>Description: </strong>
-									{item.description}
-								</p>
 								<p>
 									<strong>Price:</strong> ${item.price}
 								</p>
@@ -82,7 +44,6 @@ export const ProductList = props => {
 			</div>
 		</div>
 	);
->>>>>>> ae7830581620cc02ad92967f7d15c6b0702534ae
 };
 
 /**
--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -4,28 +4,28 @@ import { connect } from 'react-redux';
 import '../styles/sidebar.css';
 
 const Sidebar = props => {
-  let products = props.products;
-  let colors = [...new Set(products.map(product => product.color))];
-  let categories = Array.from(
-    new Set([].concat(...products.map(product => product.category)))
-  );
-  return (
-    <div className="sidebar">
-      <h2> Filter by</h2>
-      <h3> Color </h3>
-      <ul>{colors.map(color => <li key={color}> {color} </li>)}</ul>
-      <p> Category </p>
-      <ul>
-        {categories.map(category => <li key={category}> {category} </li>)}
-      </ul>
-    </div>
-  );
+	let products = props.products;
+	let colors = [...new Set(products.map(product => product.color))];
+	let categories = Array.from(
+		new Set([].concat(...products.map(product => product.category)))
+	);
+	return (
+		<div className="sidebar">
+			<h2> Filter by</h2>
+			<h3> Color </h3>
+			<ul>{colors.map(color => <li key={color}> {color} </li>)}</ul>
+			<p> Category </p>
+			<ul>
+				{categories.map(category => <li key={category}> {category} </li>)}
+			</ul>
+		</div>
+	);
 };
 
 const mapState = state => {
-  return {
-    products: state.products
-  };
+	return {
+		products: state.products
+	};
 };
 
 export default connect(mapState)(Sidebar);
