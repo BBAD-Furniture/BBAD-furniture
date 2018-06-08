0a2702c34c27d19f90bc8171390136ecc28ed9a3
David <davidgcastro93@gmail.com>
2018-06-08 17:05:46 -0400

    finished updating review route, seed file, and displayed rating on the FE (#70)

--- server/db/models/review.js
+++ server/db/models/review.js
@@ -2,7 +2,15 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const Review = db.define('review', {
-    review: Sequelize.TEXT
+  review: Sequelize.TEXT,
+  rating: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 1,
+      max: 5
+    },
+    defaultValue: null
+  }
 });
 
 module.exports = Review;
