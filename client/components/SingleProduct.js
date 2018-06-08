c0297f66c463a52fdd61af3669e890805c5754e5
Abdul Ghaffar <aghaffar570@gmail.com>
2018-06-08 00:20:14 -0400

    add items to cart (#65)

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { getCurrentProduct } from '../store';
+import { getCurrentProduct, addToCartList } from '../store';
 import { withRouter, Link } from 'react-router-dom';
 import {
   Card,
@@ -45,6 +45,9 @@ class SingleProduct extends React.Component {
               <Link to={`/products/${propsFromParent.id}`}>
                 <Button>Get Details</Button>
               </Link>
+              <button onClick={() => this.props.handleClick(propsFromParent)}>
+                Add To Cart
+              </button>
             </div>
           </CardBody>
         </Card>
@@ -62,9 +65,7 @@ const mapState = state => {
 const mapDispatch = dispatch => {
   return {
     fetchProduct: id => dispatch(getCurrentProduct(id)),
-    handleClick: item => {
-      console.log(item);
-    }
+    handleClick: item => dispatch(addToCartList(item))
   };
 };
 
