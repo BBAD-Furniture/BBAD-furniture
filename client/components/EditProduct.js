b7a2691a95543b161e1477863448bd34493e644d
David <davidgcastro93@gmail.com>
2018-06-11 15:04:32 -0400

    finished edit form load

--- client/components/EditProduct.js
+++ client/components/EditProduct.js
@@ -1,108 +1,119 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+import history from '../history';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-const EditProduct = props => {
-  let selected = props.selected ? (
-    props.selected
-  ) : (
-    <Redirect to="/addproduct" />
-  );
 
-  return (
-    <div>
-      <h1>Edit Product:</h1>
-      <Form>
-        <FormGroup className="addproduct-form">
-          <Label for="product-name">Name</Label>
-          <Input
-            required
-            type="text"
-            name="name"
-            defaultValue={selected.name}
-            placeholder={selected.name}
-          />
-          <FormText color="muted">Required*</FormText>
-        </FormGroup>
-        <FormGroup className="addproduct-form">
-          <Label for="product-color">Color</Label>
-          <Input
-            required
-            type="text"
-            name="color"
-            placeholder="Color of Product"
-            defaultValue={selected.color}
-            placeholder={selected.color}
-          />
-          <FormText color="muted">Required*</FormText>
-        </FormGroup>
-        <FormGroup className="addproduct-form">
-          <Label for="Category">Category</Label>
-          <Input
-            required
-            type="select"
-            name="category"
-            defaultValue={selected.category}
-            placeholder={selected.category}>
-            <option>Living Room</option>
-            <option>Bathroom</option>
-            <option>Bedroom</option>
-          </Input>
-          <FormText color="muted">Required*</FormText>
-        </FormGroup>
-        <FormGroup className="addproduct-form">
-          <Label for="quantity">Quantity</Label>
-          <Input
-            required
-            name="quantity"
-            type="number"
-            defaultValue={selected.quantity}
-            placeholder={selected.quantity}
-          />
-          <FormText color="muted">Required*</FormText>
-        </FormGroup>
-        <FormGroup className="addproduct-form">
-          <Label for="price">Price</Label>
-          <Input
-            required
-            name="price"
-            defaultValue={selected.price}
-            placeholder={selected.price}
-            type="number"
-          />
-          <FormText color="muted">Required*</FormText>
-        </FormGroup>
-        <FormGroup className="addproduct-form">
-          <Label for="Description">Product Description</Label>
-          <Input
-            required
-            type="textarea"
-            name="description"
-            defaultValue={selected.description}
-            placeholder={selected.description}
-          />
-          <FormText color="muted">Required*</FormText>
-        </FormGroup>
-        <FormGroup className="addproduct-form">
-          <Label for="image">Image URL</Label>
-          <Input
-            required
-            type="url"
-            name="image"
-            defaultValue={selected.image}
-            placeholder={selected.image}
-          />
-        </FormGroup>
-        <Button type="button">Submit</Button>
-      </Form>
-    </div>
-  );
-};
+class EditProduct extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {};
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit() {
+    event.preventDefault();
+    console.log('hit');
+  }
+  render() {
+    return this.props.selected ? (
+      <div>
+        <h1>Edit Product:</h1>
+        <Form>
+          <FormGroup className="addproduct-form">
+            <Label for="product-name">Name</Label>
+            <Input
+              required
+              type="text"
+              name="name"
+              defaultValue={this.props.selected.name}
+              placeholder={this.props.selected.name}
+            />
+            <FormText color="muted">Required*</FormText>
+          </FormGroup>
+          <FormGroup className="addproduct-form">
+            <Label for="product-color">Color</Label>
+            <Input
+              required
+              type="text"
+              name="color"
+              placeholder="Color of Product"
+              defaultValue={this.props.selected.color}
+              placeholder={this.props.selected.color}
+            />
+            <FormText color="muted">Required*</FormText>
+          </FormGroup>
+          <FormGroup className="addproduct-form">
+            <Label for="Category">Category</Label>
+            <Input
+              required
+              type="select"
+              name="category"
+              defaultValue={this.props.selected.category}
+              placeholder={this.props.selected.category}>
+              <option>Living Room</option>
+              <option>Bathroom</option>
+              <option>Bedroom</option>
+            </Input>
+            <FormText color="muted">Required*</FormText>
+          </FormGroup>
+          <FormGroup className="addproduct-form">
+            <Label for="quantity">Quantity</Label>
+            <Input
+              required
+              name="quantity"
+              type="number"
+              defaultValue={this.props.selected.quantity}
+              placeholder={this.props.selected.quantity}
+            />
+            <FormText color="muted">Required*</FormText>
+          </FormGroup>
+          <FormGroup className="addproduct-form">
+            <Label for="price">Price</Label>
+            <Input
+              required
+              name="price"
+              defaultValue={this.props.selected.price}
+              placeholder={this.props.selected.price}
+              type="number"
+            />
+            <FormText color="muted">Required*</FormText>
+          </FormGroup>
+          <FormGroup className="addproduct-form">
+            <Label for="Description">Product Description</Label>
+            <Input
+              required
+              type="textarea"
+              name="description"
+              defaultValue={this.props.selected.description}
+              placeholder={this.props.selected.description}
+            />
+            <FormText color="muted">Required*</FormText>
+          </FormGroup>
+          <FormGroup className="addproduct-form">
+            <Label for="image">Image URL</Label>
+            <Input
+              required
+              type="url"
+              name="image"
+              defaultValue={this.props.selected.image}
+              placeholder={this.props.selected.image}
+            />
+          </FormGroup>
+          <Button onSubmit={this.handleSubmit} type="button">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    ) : (
+      <div> {history.goBack()}</div>
+    );
+  }
+}
 
 const mapState = state => {
-  //
   return {
     selected: state.selectedProduct[0]
   };
 };
-export default connect(mapState)(EditProduct);
+export default withRouter(connect(mapState)(EditProduct));
