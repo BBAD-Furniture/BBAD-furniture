5ea5fa170f4145677eb402e54409a22b19827aed
Bushra Taimoor <bushra376@hotmail.com>
2018-06-12 00:23:38 -0400

    order info page complete

--- client/components/OrderInfo.js
+++ client/components/OrderInfo.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { getUserOrders, getUserOrdersDetails } from '../store';
+import {
+  getUserOrders,
+  getUserOrdersDetails,
+  getCurrentProduct
+} from '../store';
 import { Link } from 'react-router-dom';
 import '../styles/cart.css';
-import user from '../store/user';
 
 class OrderInfo extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-
   componentDidMount() {
     const userId = this.props.user.id;
     this.props.getUserOrders(userId);
@@ -17,15 +16,66 @@ class OrderInfo extends React.Component {
   }
 
   render() {
-    return <div>ORDER INFO PAGE</div>;
+    let { orderDetails, products, fetchProduct } = this.props;
+    orderDetails = orderDetails.reduce((a, c) => {
+      return a.concat(c);
+    }, []);
+    const id = this.props.match.params.orderId;
+    let totalPrice = 0;
+
+    return (
+      <div>
+        <h1>ORDER INFO PAGE</h1>
+        <div>
+          <div />
+          <h2> ORDER # {id}</h2>
+          <div className="shopping-cart">
+            <div className="column-labels">
+              <label>Image</label>
+              <label>Product</label>
+              <label>Price</label>
+              <label>Quantity</label>
+              <label>Total</label>
+            </div>
+            {/* for each order, find the order detail with same id*/}
+            {orderDetails.map((dets, indx) => {
+              return dets.orderId === +id ? (
+                <div key={indx}>
+                  {products.map(ele => {
+                    totalPrice += dets.price;
+                    return ele.id === dets.productId ? (
+                      <div className="product" key={ele.id}>
+                        {console.log(ele.id)}
+                        <a
+                          href={`/products/${ele.id}`}
+                          onClick={fetchProduct(id)}>
+                          <div className="product-image">
+                            <img src={ele.image} />
+                          </div>
+                        </a>
+                        <div className="product-name">{ele.name}</div>
+                        <div>${dets.price / dets.quantity}</div>
+                        <div className="quantity"> {dets.quantity}</div>
+                        <div>${dets.price}</div>
+                      </div>
+                    ) : null;
+                  })}
+                </div>
+              ) : null;
+            })}
+            <div>Grand Total :{totalPrice}</div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
 const mapState = state => {
   return {
-    orders: state.orders,
     user: state.user,
-    orderDetails: state.orderDetails
+    orderDetails: state.orderDetails,
+    products: state.products
   };
 };
 
@@ -36,7 +86,8 @@ const mapDispatch = dispatch => {
     },
     getOrderDetails: userId => {
       dispatch(getUserOrdersDetails(userId));
-    }
+    },
+    fetchProduct: id => dispatch(getCurrentProduct(id))
   };
 };
 
